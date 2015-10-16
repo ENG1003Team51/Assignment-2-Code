738673e862b2cb6648049625abdc35236f8e1ecc
KYLETENG <gten3@082-121-014WL.AD.MONASH.EDU>
2015-10-16 12:18:11 +1100

    change

--- material-design-lite-components-demo/js/record.js
+++ material-design-lite-components-demo/js/record.js
@@ -9,3 +9,72 @@ function initMap() {
 function doIt(){ //activated by button push..
     console.log("doIt() started");
 }
+
+function startRecord(){
+	var mapMode = google.maps.DirectionTravelMode.DRIVING;
+	var directionsDisplay = new google.maps.DirectionsRenderer();
+	var directionService = new google.maps.DirectionsService();
+	var directionsVisible = false;
+	
+	directionsDisplay.setMap(null);
+	directionsDisplay.setMap(map);
+	
+	var points = [];
+/*	
+	google.maps.event.addListener(map,"click",function(Event){
+		if(points.length == 0){
+			if(confirm("Setting local postion become start point?")){
+				point.push(Event.LatLng);
+			}
+		}else{
+			if(point.length >= 2){
+				point = []: google.maps.event.clearListeners(map,"click");
+				return
+			}
+			if(confirm("Setting local postion become start point?")){
+				point.push(Event.LatLng);
+				var request = {
+					origin: point[0],
+					destination: point[1],
+					travelMode: Mode,
+					optimizeWaypoints: true,
+					avoidHighways: false,
+					avoidTolls: false
+				};
+				directionsService.route(request, function(response, status){
+					if(status == google.maps.DirectionsStatus.OK){
+						directionDisplay.setDirection(response);
+						setTimeout(function(){directionDisplay.setMap(null)}, 8000)
+					}
+				});
+			}
+		}
+	});
+	directionsVisible = true;
+}
+*/
+/*
+	function setStart(){
+		var newcenter = map.getCenter();
+		if(!startPoint){
+			startPoint = new google.maps.Marker({
+				map: map,
+				position: newcenter,
+				draggable: true
+			});
+		}
+	}
+	function setEnd(){
+		var newcenter = map.getCenter();
+		if(!endPoint){
+			endPoint = new google.maps.Marker(){
+				map: map,
+				position: newcenter,
+				draggable: true
+			}
+		}
+	}
+	
+
+	*/
+}
