34df09489d4ff44694921ce4f90d319923d6d025
KYLETENG <gten3@030-101-054WL.AD.MONASH.EDU>
2015-10-20 13:55:24 +1100

    save and load idea
    Yesterday upload fail

--- Assignment-2-Code.git/LoadingLists.js
+++ Assignment-2-Code.git/LoadingLists.js
@@ -1,3 +1,24 @@
+
+function storeRouteLists(){
+
+    var routeLists = null;
+    routeLists = document.getElementById("route");
+
+
+    var saveButton = null;
+    saveButton = document.getElementById("saveButton");
+
+    var routeKey = "RouteListsLocalStorage";
+
+    
+    if (saveButton == "true"){
+        localStorage.setItem(routeKey, JSON.stringify(routeLists));
+    }else{
+        console.log("storeRouteLists() Wrong !!!")
+    }
+}
+
+
 function loadingList(){
     
     var routeList;
